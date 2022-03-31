b389b444e24e4bd7860600035edeb86e065fd178
januarydo <99490410+januarydo@users.noreply.github.com>
2022-03-31 15:48:36 +0900

    Add files via upload

---
+++ week3/tori/function.js
@@ -0,0 +1,18 @@
+function multiply10 (num) {
+  const result = num * 10;
+  return result;
+}
+
+// const data = multiply10(10);
+// console.log(data);
+
+// const result = multiply10(10);
+// console.log(result);
+
+//===========================
+//arrow function :
+
+const multiply10 = (num) => num * 10;
+
+const data = mulpiply10(100);
+console.log(data);
